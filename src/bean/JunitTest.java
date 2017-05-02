fe4a767f5b0451762cb8262c23606ad8894a54e7
BlackCompetencyBuilding <md.raghib0101@gmail.com>
2017-05-02 12:59:44 +0200

    Update JunitTest.java

--- src/bean/JunitTest.java
+++ src/bean/JunitTest.java
@@ -5,21 +5,21 @@ import org.junit.Test;
 
 public class JunitTest {
 
-		@Before
-		public void setup(){
-			setBaseUrl("http://10.0.0.116:8080/job/JunitTest1/ws/WebContent/index1.jsp");
+		//@Before
+		//public void setup(){
+		//	setBaseUrl("http://10.0.0.116:8080/job/JunitTest1/ws/WebContent/index1.jsp");
 			
-		}
+		//}
 		@Test
 		public void test(){
-			//beginAt("index1.jsp"); 
+			beginAt("http://10.0.0.116:8080/job/JunitTest1/ws/WebContent/index1.jsp"); 
 	     
 	        assertTitleEquals("Login page");
 	       // setTextField("Uname", "admin");
 	        ///setTextField("Password", "admin");
 	        //submit();
-			//clickLink("Register Now");
-	       // assertTitleEquals("Registration Form");
+			clickLink("Register Now");
+	        assertTitleEquals("Registration Form");
 		}
 		
 }
