6c593e89406d255b1563336500be6d81f6b8665b
BlackCompetencyBuilding <md.raghib0101@gmail.com>
2017-05-06 10:30:36 +0200

    Update JunitTest.java

--- src/bean/JunitTest.java
+++ src/bean/JunitTest.java
@@ -6,14 +6,10 @@ import org.junit.Test;
 
 public class JunitTest {
 
-		@Before
-		public void setup(){
-			setBaseUrl("http://10.0.0.116:8080/job/JunitTest1/ws/WebContent/");
-			//http://10.0.0.116:8080/job/JunitTest1/ws/WebContent/
-		}
+		
 		@Test
 		public void test(){
-			beginAt("index1.jsp"); 
+			beginAt("http://10.0.0.116:8080/job/JunitTest1/ws/WebContent/index1.jsp"); 
 	        
 	        assertTitleEquals("Login page");
 	       // setTextField("Uname", "admin");
