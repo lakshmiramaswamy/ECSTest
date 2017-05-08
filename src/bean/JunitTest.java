4703541723939b7a0b634227980931fd1eb1b28c
BlackCompetencyBuilding <md.raghib0101@gmail.com>
2017-05-08 11:34:54 +0530

    Update and rename VerifyTitle.java to JunitTest.java

---
+++ src/bean/JunitTest.java
@@ -0,0 +1,38 @@
+package bean;
+import static net.sourceforge.jwebunit.junit.JWebUnit.*;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+
+public class JunitTest {
+
+		/*@Before
+		public void setup(){
+			setBaseUrl("http://localhost:8081/CustomerOnBoard/");
+			//http://10.0.0.116:8080/job/JunitTest1/ws/WebContent/
+		}*/
+		@Test
+		public void testOne(){
+			beginAt("http://localhost:8081/CustomerOnBoard/index1.jsp"); 
+	        
+	        assertTitleEquals("Login page");
+	       // setTextField("Uname", "admin");
+	       // setTextField("Password", "admin");
+	      clickLink("register");
+	        //submit();
+	        assertTitleEquals("Registration Form");
+		}
+		
+		@Test
+		 public void testTwo(){
+		   WebDriver driver = new FirefoxDriver();
+		   driver.get("http://localhost:8081/CustomerOnBoard/index1.jsp");
+		   //driver.get("http://10.0.0.108:8080/job/JunitTest1/ws/WebContent/index1.jsp");
+		  Assert.assertTrue(driver.getTitle().contains("Login Page"));
+		    }
+		
+}
+
+
