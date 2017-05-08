2a549eb9b05fd73267af520ba6c02eaab83a84eb
BlackCompetencyBuilding <md.raghib0101@gmail.com>
2017-05-08 07:18:34 +0000

    Update JunitTest.java

--- src/bean/JunitTest.java
+++ src/bean/JunitTest.java
@@ -12,7 +12,7 @@ public class JunitTest {
 		}*/
 		@Test
 		public void testOne(){
-			beginAt("http://10.0.0.108:8080/job/JunitTest1/ws/WebContent//index1.jsp"); 
+			beginAt("http://localhost:8080/job/JunitTest1/ws/WebContent/index1.jsp"); 
 	        
 	        assertTitleEquals("Login page");
 	       // setTextField("Uname", "admin");
