f454285f8b61f7b69ab86f7cca6819adc26fa78b
BlackCompetencyBuilding <md.raghib0101@gmail.com>
2017-05-10 17:18:20 +0530

    Update JunitTest.java

--- src/bean/JunitTest.java
+++ src/bean/JunitTest.java
@@ -20,7 +20,8 @@ public class JunitTest {
 		
 		@Test
 		public void testTwo(){ 
-			System.setProperty("webdriver.gecko.driver","C:\\Program Files\\geckodriver.exe");
+			System.setProperty("webdriver.gecko.driver","C:\\Program Files (x86)\\Mozilla Firefox.exe");
+			//System.setProperty("webdriver.gecko.driver","C:\\Program Files\\geckodriver.exe");
 		   WebDriver driver = new FirefoxDriver();
 			//File file = new File("C:\\Program Files\\IEDriverServer.exe");
 			//System.setProperty("webdriver.ie.driver",file.getAbsolutePath());
