389bb60702cbb22f8bc96ecc4d5c08098cae1d8e
pyelika <pyelika@192.168.235.1>
2017-05-11 13:45:31 +0530

    change

--- src/bean/JunitTest.java
+++ src/bean/JunitTest.java
@@ -1,29 +1,42 @@
 package bean;
-import static net.sourceforge.jwebunit.junit.JWebUnit.*;
+
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxBinary;
-import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.safari.SafariDriver;
 //import org.testng.annotations.Test;
 import org.testng.Assert;
 
-import com.gargoylesoftware.htmlunit.javascript.host.file.File;
-
-import org.junit.Test;
-
 public class JunitTest {
-    private WebDriver driver = null;
-    @Test
-         public void testTwo(){
+	public static WebDriver driver = null;
 
-FirefoxBinary binary = new FirefoxBinary(new File("C:\\Program Files\\Mozilla Firefox\\firefox.exe")); // Change actual firefox path
-    binary.setEnvironmentProperty("DISPLAY",System.getProperty("lmportal.xvfb.id",":99"));
-    driver = new FirefoxDriver(binary,null);
-          driver.get("http://localhost:8081/CustomerOnBoard/index1.jsp");
-          Assert.assertTrue(driver.getTitle().contains("Login Page"));
-            }
-		
-		     }
+	@Test
+	public void testTwo() {
+		System.setProperty("webdriver.gecko.driver", "C:\\Program Files\\Mozilla Firefox\\firefox.exe");
+		driver = new FirefoxDriver();
+		driver.get("http://localhost:8081/CustomerOnBoard/index1.jsp");
+		// driver.get("http://10.0.0.108:8080/job/JunitTest1/ws/WebContent/index1.jsp");
+		Assert.assertTrue(driver.getTitle().contains("Login Page"));
+	}
 
+	// public static void main(String[] args){
 
+	// String url =
+	// "http://localhost:8080/job/Selenium/ws/WebContent/index1.jsp";
+
+	// @Test
+	// public void testTwo(){
+	// System.setProperty("webdriver.gecko.driver","C:\\Program Files
+	// (x86)\\Mozilla Firefox\\firefox.exe");
+
+	// System.setProperty("webdriver.gecko.driver","C:\\Program
+	// Files\\geckodriver.exe");
+	// WebDriver driver = new FirefoxDriver();
+	// File file = new File("C:\\Program Files\\IEDriverServer.exe");
+	// System.setProperty("webdriver.ie.driver",file.getAbsolutePath());
+	// WebDriver driver = new InternetExplorerDriver();
+	// System.setProperty("webdriver.ie.driver", "IEDriverServer.exe");
+	// driver.get("http://localhost:8080/job/Selenium/ws/WebContent/index1.jsp");
+	// driver.get("http://localhost:8080/job/Selenium/ws/WebContent/index1.jsp");
+	// Assert.assertTrue(driver.getTitle().contains("Login Page"));
+	// }
+}
