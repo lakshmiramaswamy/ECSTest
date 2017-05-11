744c2ceea88e171dc35096dac344b34b613e21af
BlackCompetencyBuilding <md.raghib0101@gmail.com>
2017-05-11 11:49:57 +0530

    Update JunitTest.java

--- src/bean/JunitTest.java
+++ src/bean/JunitTest.java
@@ -12,26 +12,41 @@ import com.gargoylesoftware.htmlunit.javascript.host.file.File;
 import org.junit.Test;
 
 public class JunitTest {
+	
+	@Test
+		 public void testTwo(){
+		   WebDriver driver = new FirefoxDriver();
+		   driver.get("http://localhost:8081/CustomerOnBoard/index1.jsp");
+		   //driver.get("http://10.0.0.108:8080/job/JunitTest1/ws/WebContent/index1.jsp");
+		  Assert.assertTrue(driver.getTitle().contains("Login Page"));
+		    }
+	
+	
+	
+	
+	
+	
+	
 	//public static void main(String[] args){
 		
 		
 		//String url = "http://localhost:8080/job/Selenium/ws/WebContent/index1.jsp";
 		
 		
-		@Test
-		public void testTwo(){ 
-			System.setProperty("webdriver.gecko.driver","C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe");
+		//@Test
+		//public void testTwo(){ 
+			//System.setProperty("webdriver.gecko.driver","C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe");
 			
 			//System.setProperty("webdriver.gecko.driver","C:\\Program Files\\geckodriver.exe");
-		   WebDriver driver = new FirefoxDriver();
+		   //WebDriver driver = new FirefoxDriver();
 			//File file = new File("C:\\Program Files\\IEDriverServer.exe");
 			//System.setProperty("webdriver.ie.driver",file.getAbsolutePath());
 			//WebDriver driver = new InternetExplorerDriver();
 			//System.setProperty("webdriver.ie.driver", "IEDriverServer.exe"); 
 		  // driver.get("http://localhost:8080/job/Selenium/ws/WebContent/index1.jsp");
-		   	driver.get("http://localhost:8080/job/Selenium/ws/WebContent/index1.jsp");
-		 	Assert.assertTrue(driver.getTitle().contains("Login Page"));
-		 }
+		   	//driver.get("http://localhost:8080/job/Selenium/ws/WebContent/index1.jsp");
+		 	//Assert.assertTrue(driver.getTitle().contains("Login Page"));
+		// }
 }
 		
 
