57a0ae908848407b788ec9893ad3e615c824ead0
BlackCompetencyBuilding <md.raghib0101@gmail.com>
2017-05-12 12:07:36 +0530

    Update JunitTest.java

--- src/bean/JunitTest.java
+++ src/bean/JunitTest.java
@@ -18,9 +18,8 @@ public class JunitTest {
 		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
 		capabilities.setCapability("marionette", false);
 		driver = new FirefoxDriver(capabilities);
-		driver.get("https://www.google.co.in");
 		//driver.get("http://localhost:8080/CustomerOnBoard/index1.jsp");
-		//driver.get("http://10.0.0.108:8080/job/JunitTest1/ws/WebContent/index1.jsp");
+		driver.get("http://10.0.0.108:8080/job/JunitTest1/ws/WebContent/index1.jsp");
 		//Assert.assertTrue(driver.getTitle().contains("Login page"));
 	}
 
