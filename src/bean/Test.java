037362fd277cee62afda62f777c703afb0ee5c19
lakshmiramaswamy <35726171+lakshmiramaswamy@users.noreply.github.com>
2018-02-11 13:31:57 +0530

    Update Test.java

--- src/bean/Test.java
+++ src/bean/Test.java
@@ -42,6 +42,7 @@ public class Test{
 		String dbName = "MyDatabaseBlack";
 		String driver = "com.mysql.jdbc.Driver";
 		//Connection connection = DriverManager.getConnection(url + dbName, userName, password);
+		Class.forName(driver);
 
 		Connection cx = DriverManager.getConnection(url + dbName, userName, password);
 		 //cx = dataSource.getConnection();
